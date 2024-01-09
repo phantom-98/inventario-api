b40dbd0209bf35095b6f1c47b7ffd79a28a0fb33
samuelborraccini <samuelborraccini@hotmail.com>
2024-01-09 09:40:53 -0300

    init feature

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -4,6 +4,7 @@ import User from "../models/User.js";
 
 const checkAuth = async (req, res, next) => {
     const token = req.header('x-token');
+    console.log(token);
     if( !token  ) {
         return res.status(401).json({
             ok: false,
@@ -16,7 +17,8 @@ const checkAuth = async (req, res, next) => {
         const { uid } = jwt.verify( token, `${process.env.JWT_SECRET}` );
         if(uid !== "Web"){
             req.uid  = uid;
-            req.user = await User.findOne({ _id:uid });
+            console.log(uid);
+            //req.user = await User.findOne({ _id:uid });
         }
         
 
