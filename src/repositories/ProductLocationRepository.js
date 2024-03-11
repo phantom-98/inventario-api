15afef3b32f30bfc44ca83b01942b8f5143ea842
samuelborraccini <samuelborraccini@hotmail.com>
2024-03-11 12:57:36 -0300

    fix prod location

--- src/repositories/ProductLocationRepository.js
+++ src/repositories/ProductLocationRepository.js
@@ -2,24 +2,24 @@ import prisma from "../db/index.js";
 
 class ProductLocationRepository {
   getProdLocation = async (id) => {
-    const locations = prisma.location_product.findMany({
+    const locations = await prisma.location_product.findMany({
       where: { product_id: id },
       include: { locations: true },
     });
     return locations;
   };
   getAll = async () => {
-    const locations = prisma.locations.findMany();
+    const locations = await prisma.locations.findMany();
     return locations;
   };
   createProdLocation = async (productId, locationId) => {
-    const productLocation = prisma.location_product.create({
+    const productLocation = await prisma.location_product.create({
       data: { product_id: productId, location_id: locationId },
     });
     return productLocation;
   };
   deleteById = async (id) => {
-    const removeLoc = prisma.location_product.delete({
+    const removeLoc = await prisma.location_product.delete({
       where: { id: id },
     });
     return removeLoc;
